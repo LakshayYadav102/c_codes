1df99b788d6285acf1dcf945d7ff0b2b4498fc58
LakshayYadav102 <141443288+LakshayYadav102@users.noreply.github.com>
2023-08-07 12:35:26 +0530

    Add files via upload

---
+++ to_print_sum_from_5to50.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+// SUM OF ALL NUMBER FROM 5 TO 50
+
+int main() {
+    int sum=0;
+  for(int i=5;i<=50;i++){
+    sum=sum+i;
+  }
+  printf("sum is %d",sum);
+  return 0;
+}
