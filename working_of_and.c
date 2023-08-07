05e3713a02fe57a2d5d932a21b4e75ab62d87438
LakshayYadav102 <141443288+LakshayYadav102@users.noreply.github.com>
2023-08-07 09:32:43 +0530

    Add files via upload

---
+++ working_of_and.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+//iss sunday and snowing
+int main() {
+    int issunday=1;
+    int issnowing=1;
+    printf("%d",issunday&&issnowing);
+    return 0;
+}
---
+++ working_of_or.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+//itsmonday and itsraining
+
+int main() {
+    int ismonday=0;
+    int israining=1;
+    printf("%d",ismonday || israining);
+    return 0;
+
+}
